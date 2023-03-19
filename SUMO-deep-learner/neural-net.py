cf7fc9e9f5ad09454af4969450372fdfee46e059
Tadhg McCarthy <82101533+tadhgmcc77@users.noreply.github.com>
2023-03-19 20:28:57 +0000

    Add files via upload

---
+++ SUMO-deep-learner/neural-net.py
@@ -0,0 +1,79 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'  # kill warning about tensorflow
+import tensorflow as tf
+import numpy as np
+import sys
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.models import load_model
+
+
+class TrainModel:
+    def __init__(self, num_layers, width, batch_size, learning_rate, input_dimensions, output_dimensions):
+        self._input_dimensions = input_dimensions
+        self._output_dimensions = output_dimensions
+        self._batch_size = batch_size
+        self._learning_rate = learning_rate
+        self._neural_net = self._build_model(num_layers, width)
+
+
+    def _build_model(self, num_layers, width):
+        """
+        Build and compile a fully connected deep neural network
+        """
+        inputs = keras.Input(shape=(self._input_dim,))
+        x = layers.Dense(width, activation='relu')(inputs)
+        for _ in range(num_layers):
+            x = layers.Dense(width, activation='relu')(x)
+        outputs = layers.Dense(self._output_dim, activation='linear')(x)
+
+        model = keras.Model(inputs=inputs, outputs=outputs, name='my_model')
+        model.compile(loss=losses.mean_squared_error, optimizer=Adam(lr=self._learning_rate))
+        return model
+    
+
+    def predict_one(self, state):
+        """
+        Predict the action values from a single state
+        """
+        state = np.reshape(state, [1, self._input_dim])
+        return self._model.predict(state)
+
+
+    def predict_batch(self, states):
+        """
+        Predict the action values from a batch of states
+        """
+        return self._model.predict(states)
+
+
+    def train_batch(self, states, q_sa):
+        """
+        Train the nn using the updated q-values
+        """
+        self._model.fit(states, q_sa, epochs=1, verbose=0)
+
+
+    def save_model(self, path):
+        """
+        Save the current model in the folder as h5 file and a model architecture summary as png
+        """
+        self._model.save(os.path.join(path, 'traffic_model.h5'))
+
+
+
+    def input_dim(self):
+        return self._input_dim
+
+
+    def output_dim(self):
+        return self._output_dim
+
+
+    def batch_size(self):
+        return self._batch_size
+

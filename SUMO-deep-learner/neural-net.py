254c9d5e155382604ff7a0f8c170b53a8251c49f
tadhgmcc77 <tadhg.mcc77@gmail.com>
2023-03-21 14:44:55 +0000

    neural_net_batch_predict

--- SUMO-deep-learner/neural-net.py
+++ SUMO-deep-learner/neural-net.py
@@ -11,10 +11,8 @@ from keras.models import load_model
 
 class TrainNeuralNet:
     def __init__(self, num_layers, input_dimensions, output_dimensions, width, batch_size, learning_rate):
-        self._num_layers = num_layers
         self.input_dimemsions = input_dimensions
         self._output_dimensions = output_dimensions
-        self._width = width
         self._batch_size = batch_size
         self._learning_rate = learning_rate
         self._neural_net = self.build_neural_net(num_layers, width)
@@ -30,11 +28,22 @@ class TrainNeuralNet:
         # No.outputs is = number of possible actions to take | activation is linear because we are values (regression)
         outputs = layers.Dense(self._output_dimensions, activation='linear')(x)
 
-        network = keras.Model(inputs=inputs, outputs=outputs, name='model')
-        network.compile(loss=losses.mean_squared_error, optimizer=Adam(lr=self._learning_rate))
-        return network
+        neural_net = keras.Model(inputs=inputs, outputs=outputs, name='model')
+        neural_net.compile(loss=losses.mean_squared_error, optimizer=Adam(lr=self._learning_rate))
+        return neural_net
     
     def predict_single(self, state):
         state = np.reshape(state, [1, self.input_dimemsions])
-        
+        return self._neural_net.predict(state)
     
+    def predict_batch(self, states):
+        return self._neural_net.predict(states)
+    
+    def train_batch(self, states, updated_Q):
+        self._neural_net.fit(states, updated_Q, epochs=1)
+
+    def save_nerual_net(self, filepath):
+        self._model.save(os.path.join(filepath, 'trained_model.h5'))
+        plot_model(self._model, to_file=os.path.join(filepath, 'model_structure.png'), show_shapes=True, show_layer_names=True)
+
+        
